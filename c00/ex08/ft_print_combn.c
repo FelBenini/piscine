1ed59f255f190a9ac8c3bf7cb5f61dcf79121491
FelBenini <felbenini@gmail.com>
2025-05-18 10:35:49 -0300

    Optimizing a little bit

--- c00/ex08/ft_print_combn.c
+++ c00/ex08/ft_print_combn.c
@@ -6,7 +6,7 @@
 /*   By: fbenini- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 12:29:52 by fbenini-          #+#    #+#             */
-/*   Updated: 2025/05/18 10:22:53 by fbenini-         ###   ########.fr       */
+/*   Updated: 2025/05/18 10:35:22 by fbenini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,32 +37,36 @@ void	ft_convert_int_to_char(int n, char *c)
 {
 	int	i;
 	int	temp;
+	int	len;
 
 	temp = n;
-	i = intlen(n) - 1;
+	len = intlen(n);
+	i = len - 1;
 	while (i >= 0)
 	{
 		c[i] = (temp % 10) + '0';
 		temp /= 10;
 		i--;
 	}
-	c[intlen(n)] = '\0';
+	c[len] = '\0';
 }
 
 void	ft_print_formatted(int n, int n_digits, int *is_first)
 {
 	int		i;
 	char	c[12];
+	int		is_valid;
 
 	ft_convert_int_to_char(n, c);
+	is_valid = ft_is_ascendant(c);
 	i = 0;
 	if ((intlen(n) < n_digits - 1) || (n_digits == 2 && n == 0))
 		return ;
-	if (!*is_first && ft_is_ascendant(c))
+	if (!*is_first && is_valid)
 		write(1, ", ", 2);
-	if (intlen(n) == n_digits - 1 && ft_is_ascendant(c))
+	if (intlen(n) == n_digits - 1 && is_valid)
 		write(1, "0", 1);
-	while (i < n_digits && ft_is_ascendant(c) && c[i])
+	while (i < n_digits && is_valid && c[i])
 	{
 		write(1, &c[i], 1);
 		*is_first = 0;
@@ -74,11 +78,20 @@ void	ft_print_combn(int n)
 {
 	int	i;
 	int	is_first;
+	int	max;
+	int	j;
 
 	i = 0;
+	max = 1;
+	j = 0;
 	is_first = 1;
 	if (n <= 0 || n >= 10)
 		return ;
+	while (j < n)
+	{
+		max *= 10;
+		j++;
+	}
 	while (intlen(i) <= n)
 	{
 		if (i > 123456789)
@@ -90,5 +103,5 @@ void	ft_print_combn(int n)
 //
 // int	main(void)
 // {
-// 	ft_print_combn(8);
+// 	ft_print_combn(9);
 // }
